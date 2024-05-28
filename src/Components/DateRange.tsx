443d28f3c07a7c5faded10306531bf104508fdee
Divino <josedivinooficial02@gmail.com>
2024-05-28 20:05:54 -0300

    Project Fintech

---
+++ src/Components/DateRange.tsx
@@ -0,0 +1,23 @@
+import { useData } from '../Context/DataContext';
+import DateInput from './DateInput';
+
+const DateRange = () => {
+  const { inicio, final, setFinal, setInicio } = useData();
+
+  return (
+    <form className="box flex" onSubmit={e => e.preventDefault()}>
+      <DateInput
+        label="Inicio"
+        value={inicio}
+        onChange={({ target }) => setInicio(target.value)}
+      />
+      <DateInput
+        label="Final"
+        value={final}
+        onChange={({ target }) => setFinal(target.value)}
+      />
+    </form>
+  );
+};
+
+export default DateRange;
